56b4fd1f1e90b1c9926395f3f0247f036f836d47
Marwenda <78843889+Marwenda@users.noreply.github.com>
2021-11-09 16:52:42 +0100

    Tablas unidas en Tabla.sql

--- Tablas.sql
+++ Tablas.sql
@@ -26,4 +26,28 @@ Capacitat int ,
 primary key (ID)
 foreign key (ejerciciosnorepetir) REFERENCES EJERCICIOS(ID)
 )engine=innodb;
+CREATE TABLE TABLA_EJERCICIOS (
+ID int not null unique,
+ID_Cliente int,
+Fecha date,
+ID_Ejercicios int,
+Nombre_Ejercicio varchar(50),
+Series_Ejercicio int,
+Repeticiones_por_serie int,
+primary key (ID, ID_Cliente),
+foreign key (ID_Cliente) references CLIENTES(ID)
+foreign key (ID_Ejercicio) references EJERCICIOS(ID)
+foreign key (Nombre_Ejercicio) references EJERCICIOS(Nombre)
+)engine=innodb;
 
+CREATE TABLE FEEDBACK (
+N_Feedback int not null unique,
+Cansancio int,
+G_Satisfaccion int,
+Cometnario varchar(500),
+ID_Cliente int not null unique,
+ID_Tabla int not null unique,
+primary key (N_Feedback),
+foreign key (ID_Cliente) references CLIENTES(ID),
+foreign key (ID_Tabla) references TABLAS_EJERCICIOS(ID)
+)engine=innodb;
