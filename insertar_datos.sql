adcfd547d3cf9d87601487fb1fe6ffdfc411e54a
Marwenda <78843889+Marwenda@users.noreply.github.com>
2021-11-16 16:42:11 +0100

    Update insertar_datos.sql

--- insertar_datos.sql
+++ insertar_datos.sql
@@ -13,6 +13,21 @@ VALUES  (1,"Miriru","Password","Maria Irun","maria@bemen3.cat",25,51.3,165.2,"Tr
         (4,"J","MenInBlack","Will Smith","WSmithy@gmail.com",55,88,192,"",FALSE),
         (5,"Captain","America","Chris Evans","1stAvenger@gmail.com",42,95,197,"",FALSE);
 
+/*------------Inserció de dades ----------*/
+INSERT INTO TABLA_EJERCICIOS
+VALUES (1, 1, '2021-10-15', 'Press hombro 4 series por 12 repeticiones #  Press banca 3 series por 11 repetiiopnes # Sentadilla con peso 3 series por 21 repeticiones # '),
+        (2, 2, '2021-10-16', 'Curl biceps 5 series por 10 repeticiones # Sentadilla con peso 3 series por 21 repeticiones # Trotar 20 minutos ritmo relajado'),
+        (3, 3, '2021-10-17', 'Sentadilla con peso 3 series por 21 repeticiones # Trotar 20 minutos ritmo relajado # Press banca 3 series por 11 repetiiopnes'),
+        (4, 4, '2021-10-18', 'Trotar 20 minutos ritmo relajado # Press banca 3 series por 11 repetiiopnes # Press hombro 4 series por 12 repeticiones # Curl biceps 5 series por 10 repeticiones'),
+        (5, 5, '2021-10-19', 'Press banca 3 series por 11 repetiiopnes # Curl biceps 5 series por 10 repeticiones # Sentadilla con peso 3 series por 21 repeticiones');
+
+INSERT INTO FEEDBACK
+VALUES (1, 10, 9, 'Me ha encantado el Curl biceps!', 2, 2),
+        (2, 8, 10, 'Me ha encantado, una tabla completisima!', 1, 1),
+        (3, 6, 6, 'No me ha gustado mucho, un poco repetitiva!', 3, 3),
+        (4, 10, 5, 'Demasiado dura, estoy rebentado..!', 5, 5),
+        (5, 10, 4, 'No me ha gustado... una tabla poco completa...!', 4, 4);
+
 
 
 
