bcce5df0c3cb334085284b00df6f4cfcc20a55f8
marwenda <marwenda55@gmail.com>
2021-11-16 16:17:33 +0100

    Cambios tablas y datos de Ejercicios y Clientes

---
+++ insertar_datos.sql
@@ -0,0 +1,21 @@
+INSERT INTO EJERCICIOS(ID,Nombre,Aerobico,GrupoMuscular,HIT,Gimnasio)
+VALUES  (1,"Press Banca", FALSE, "Torso" , FALSE, TRUE ),
+        (2,"Press Hombro", FALSE, "Torso" , FALSE, TRUE ),
+        (3,"Curl de Biceps", FALSE, "Torso" , FALSE, TRUE ),
+        (4,"Sentadilla con peso", FALSE, "Piernas" , FALSE, TRUE ),
+        (5,"Trotar", TRUE, "Full Body" , TRUE, FALSE ) ;
+
+
+INSERT INTO CLIENTES(ID, Alias,Pass,Nombre_apell,Email,Edad,Peso,Altura,Ejercicios_norepetir,Mov_reducida)
+VALUES  (1,"Miriru","Password","Maria Irun","maria@bemen3.cat",25,51.3,165.2,"Trotar",FALSE),
+        (2,"Maruchan","contraseña","Kathy Perry","Kathyper@gmail.com",27,55.3,175,"Sentadilla",FALSE),
+        (3,"Gordito feliz","Pastel","Antonio Julian","Felicito@hotmail.com",31,95.8,185.2,"",TRUE), 
+        (4,"J","MenInBlack","Will Smith","WSmithy@gmail.com",55,88,192,"",FALSE),
+        (5,"Captain","America","Chris Evans","1stAvenger@gmail.com",42,95,197,"",FALSE);
+
+
+
+
+
+
+
